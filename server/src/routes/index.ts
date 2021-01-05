690ed1e6bf7f78596abb5d86017bdf36241cfcbc
ElCholoGamer <josedanielgrayson@gmail.com>
2021-01-05 18:29:03 -0500

    whoa it was this simple

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -9,13 +9,7 @@ router.use(express.static(BUILD));
 const indexHtml = join(BUILD, 'index.html');
 
 router.get('*', (req, res, next) => {
-	const { method, headers } = req;
-
-	if (
-		method.toUpperCase() === 'GET' &&
-		headers.accept?.indexOf('text/html') !== -1 &&
-		!req.path.startsWith('/api')
-	) {
+	if (req.headers.accept?.indexOf('text/html') !== -1) {
 		res.sendFile(indexHtml);
 	} else {
 		next();
