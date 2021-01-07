ad5eb93ed89214362c0d7470cd73c52c7ed31763
ElCholoGamer <josedanielgrayson@gmail.com>
2021-01-06 23:06:09 -0500

    fix a weird typo

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -16,7 +16,7 @@ if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
 	window.addEventListener('load', () => {
 		navigator.serviceWorker
 			.register('/service-worker.js')
-			.then(reg => console.log('SW registration succeede. Scope:', reg.scope))
+			.then(reg => console.log('SW registration succeeded. Scope:', reg.scope))
 			.catch(err => console.log('SW registration failed: ', err));
 	});
 }
