c38e8bf53fce7508eb058a91c4afe8c06f41a6fa
ElCholoGamer <josedanielgrayson@gmail.com>
2021-01-07 21:41:00 -0500

    add app routes and create _variables.scss

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.scss';
 
+const Channels = lazy(() => import('./pages/Channels'));
+const Home = lazy(() => import('./pages/Home'));
+const Login = lazy(() => import('./pages/Login'));
+const Register = lazy(() => import('./pages/Register'));
+
 const App: React.FC = () => {
-	return <div>App component</div>;
+	return (
+		<Suspense fallback={<h2>Loading...</h2>}>
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route exact path="/channels" component={Channels} />
+				<Route exact path="/login" component={Login} />
+				<Route exact path="/register" component={Register} />
+
+				<Redirect to="/" />
+			</Switch>
+		</Suspense>
+	);
 };
 
 export default App;
