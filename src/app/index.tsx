79674f6059d8e1acb4d64a08eabae9ae7d525f2b
ElCholoGamer <josedanielgrayson@gmail.com>
2021-01-11 15:05:57 -0500

    think i fixed service workers

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -12,11 +12,29 @@ render(
 	document.getElementById('root')
 );
 
-// if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
-// 	window.addEventListener('load', () => {
-// 		navigator.serviceWorker
-// 			.register('/service-worker.js')
-// 			.then(reg => console.log('SW registration succeeded. Scope:', reg.scope))
-// 			.catch(err => console.log('SW registration failed: ', err));
-// 	});
-// }
+if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
+	window.addEventListener('load', () => {
+		navigator.serviceWorker
+			.register('/service-worker.js')
+			.then(reg => {
+				console.log('SW registered with scope:', reg.scope);
+
+				reg.addEventListener('updatefound', () => {
+					const installingWorker = reg.installing;
+					installingWorker?.addEventListener('statechange', () => {
+						if (installingWorker.state !== 'installed') return;
+
+						if (navigator.serviceWorker.controller) {
+							console.log(
+								'Dankcord has been updated. This page will refresh in 5 seconds.'
+							);
+							setTimeout(() => location.reload(), 5000);
+						} else {
+							console.log('Dankcord is cached for offline use.');
+						}
+					});
+				});
+			})
+			.catch(err => console.log('SW registration failed:', err));
+	});
+}
