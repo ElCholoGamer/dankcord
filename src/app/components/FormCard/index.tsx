8a19bb5d59f98766682e4efec2fb0b1d3b6fc96b
ElCholoGamer <josedanielgrayson@gmail.com>
2021-01-11 11:40:07 -0500

    add <FormCard /> component for auth forms

---
+++ src/app/components/FormCard/index.tsx
@@ -0,0 +1,93 @@
+import React, {
+	ChangeEvent,
+	ComponentProps,
+	Dispatch,
+	MouseEvent,
+	SetStateAction,
+} from 'react';
+import { Link } from 'react-router-dom';
+import './FormCard.scss';
+
+interface Field<T> {
+	type: 'text' | 'password';
+	label: string;
+	value: keyof T & string;
+	placeholder?: string;
+	transform?(value: string): string;
+	required?: boolean;
+}
+
+interface Props<T> {
+	title: string;
+	alert?: string;
+	data: T;
+	setData: Dispatch<SetStateAction<T>>;
+	onSubmit(event: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>): void;
+	footer?: {
+		text: string;
+	} & ComponentProps<Link>;
+	fields: Field<T>[];
+}
+
+function FormCard<T extends { [key: string]: string }>({
+	title,
+	alert,
+	onSubmit,
+	data,
+	setData,
+	fields,
+	footer,
+}: Props<T>) {
+	const handleChange = (
+		e: ChangeEvent<HTMLInputElement>,
+		field: Field<any>
+	) => {
+		const { name, value } = e.target;
+		const final = field.transform ? field.transform(value) : value;
+
+		setData({ ...data, [name]: final });
+	};
+
+	return (
+		<div className="form-card">
+			<h2>{title}</h2>
+			<hr />
+
+			{fields.map((field, index) => {
+				const { value, label, type } = field;
+				return (
+					<div className="form-group" key={index}>
+						<label htmlFor={value}>{label}:</label>
+						<br />
+
+						<input
+							type={type}
+							name={value}
+							id={value}
+							value={data[value]}
+							onChange={e => handleChange(e, field)}
+						/>
+					</div>
+				);
+			})}
+
+			{alert && <p className="alert-text">{alert}</p>}
+
+			<button
+				onClick={onSubmit}
+				className="btn form-btn"
+				disabled={fields.some(field => field.required && !data[field.value])}>
+				Log In
+			</button>
+
+			<br />
+			{footer && (
+				<Link className="card-link" {...footer}>
+					{footer.text}
+				</Link>
+			)}
+		</div>
+	);
+}
+
+export default FormCard;
---
+++ src/app/pages/Login.tsx
@@ -0,0 +1,68 @@
+import React, { Dispatch, MouseEvent, SetStateAction, useState } from 'react';
+import axios, { AxiosError } from 'axios';
+import { useHistory } from 'react-router-dom';
+import FormCard from '@components/FormCard';
+
+interface Props {
+	setLoaded: Dispatch<SetStateAction<boolean>>;
+}
+
+const Login: React.FC<Props> = ({ setLoaded }) => {
+	const history = useHistory();
+	const [alert, setAlert] = useState('');
+	const [input, setInput] = useState({ username: '', password: '' });
+
+	const handleClick = (
+		e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>
+	) => {
+		e.preventDefault();
+
+		const btn = e.currentTarget;
+		btn.disabled = true;
+
+		axios
+			.post('/auth/login', input)
+			.then(() => {
+				setLoaded(false);
+				history.push('/');
+			})
+			.catch((err: AxiosError) => {
+				setAlert(err.response?.data?.message || 'Unknown error.');
+				console.error(err);
+				btn.disabled = false;
+			});
+	};
+
+	return (
+		<main>
+			<FormCard
+				title="Welcome back!"
+				data={input}
+				setData={setInput}
+				onSubmit={handleClick}
+				fields={[
+					{
+						label: 'Username',
+						value: 'username',
+						type: 'text',
+						required: true,
+						transform: s => s.trimStart(),
+					},
+					{
+						label: 'Password',
+						value: 'password',
+						type: 'password',
+						required: true,
+					},
+				]}
+				alert={alert}
+				footer={{
+					text: "Don't have an account yet?",
+					to: '/register',
+				}}
+			/>
+		</main>
+	);
+};
+
+export default Login;
