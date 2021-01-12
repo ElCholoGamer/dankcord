25850d4918895e56ca91d8d74e2a5f6eaeeff8da
ElCholoGamer <josedanielgrayson@gmail.com>
2021-01-11 19:53:05 -0500

    add channels button to home

--- src/app/pages/Home/index.tsx
+++ src/app/pages/Home/index.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import Logo from '@assets/images/logo.png';
 import './Home.scss';
+import { useHistory } from 'react-router-dom';
 
 const Home: React.FC = () => {
-	return (
-		<main>
-			<div id="home-content">
-				<img src={Logo} alt="Dankcord logo" />
+	const history = useHistory();
 
-				<h1 id="home-title">Your place NOT to talk</h1>
-				<p>
-					Seriously tho, this app is just some random Discord clone anyways.
-					Haven't you noticed yet?
-				</p>
-			</div>
+	return (
+		<main id="home-main">
+			<img src={Logo} alt="Dankcord logo" />
+
+			<h1 id="home-title">Your place NOT to talk</h1>
+			<p>
+				Seriously tho, this app is just some random Discord clone anyways.
+				Haven't you noticed yet?
+			</p>
+
+			<button
+				onClick={() => history.push('/channels')}
+				className="btn"
+				id="channels-btn">
+				Channels
+			</button>
 		</main>
 	);
 };
