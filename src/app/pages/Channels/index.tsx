7d9c75a24c71b781ab0e8d310277ce6dba9a9670
ElCholoGamer <josedanielgrayson@gmail.com>
2021-01-14 00:07:14 -0500

    add messages input and fix some styling

--- src/app/pages/Channels/index.tsx
+++ src/app/pages/Channels/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, KeyboardEvent, useState, useRef } from 'react';
 import { Redirect } from 'react-router-dom';
 import axios from 'axios';
 import { Channel, Message, User } from '../../util/structures';
@@ -13,13 +13,38 @@ interface Props {
 }
 
 const Channels: React.FC<Props> = ({ user }) => {
+	const inputRef = useRef<HTMLInputElement>(null);
+	const messagesRef = useRef<HTMLDivElement>(null);
+
 	const [connected, setConnected] = useState(false);
 	const [users, setUsers] = useState<User[]>([]);
 	const [channels, setChannels] = useState<ChannelState>({});
 	const [selected, setSelected] = useState('');
 	const [messages, setMessages] = useState<Record<string, Message[]>>({});
+	const [input, setInput] = useState('');
 
-	// Set connected when channels are available
+	const scrollMessages = () => {
+		const messagesDiv = messagesRef.current;
+		if (messagesDiv) messagesDiv.scrollTop = messagesDiv.scrollHeight;
+	};
+
+	const handleKeyPress = (e: KeyboardEvent<HTMLInputElement>) => {
+		if (e.key !== 'Enter' || !input) return;
+
+		const field = e.currentTarget;
+		field.disabled = true;
+		setInput('');
+
+		axios
+			.post(`/api/channels/${selected}/messages`, { content: input })
+			.catch(console.error)
+			.finally(() => {
+				field.disabled = false;
+				inputRef.current?.focus();
+			});
+	};
+
+	// Select default channel when connected
 	useEffect(() => {
 		if (!connected) return;
 
@@ -30,13 +55,18 @@ const Channels: React.FC<Props> = ({ user }) => {
 	// Update messages when selected changes
 	useEffect(() => {
 		if (selected) localStorage.setItem('lastChannel', selected);
-		if (!selected || selected in messages) return;
+		if (!selected) return;
+
+		// Fetch selected channel messages
+		if (selected in messages) return scrollMessages();
 
 		let timer: ReturnType<typeof setTimeout> | null = null;
 		(async function fetchMessages() {
 			try {
 				const res = await axios.get(`/api/channels/${selected}/messages`);
 				setMessages(prev => ({ ...prev, [selected]: res.data }));
+
+				scrollMessages();
 			} catch {
 				timer = setTimeout(fetchMessages, 1000);
 			}
@@ -47,6 +77,7 @@ const Channels: React.FC<Props> = ({ user }) => {
 		};
 	}, [selected]);
 
+	// Component mounted
 	useEffect(() => {
 		if (!user) return;
 
@@ -58,8 +89,9 @@ const Channels: React.FC<Props> = ({ user }) => {
 		const url = `${secure ? 'wss' : 'ws'}://${host}/gateway`;
 
 		const ws = new WebSocket(url);
-		ws.addEventListener('message', async (event: MessageEvent<any>) => {
+		ws.addEventListener('message', async event => {
 			const { e, d } = JSON.parse(event.data);
+			console.log('e:', e, 'd:', d);
 
 			switch (e) {
 				case 'READY':
@@ -90,12 +122,12 @@ const Channels: React.FC<Props> = ({ user }) => {
 					setUsers(prev => prev.filter(user => user.id !== d.id));
 					break;
 				case 'MESSAGE_CREATE':
-					const { channel } = d;
 					setMessages(prev => {
-						if (!(channel in prev)) prev[channel] = [];
-						prev[channel].push(d);
-						return prev;
+						const list = prev[d.channel] ?? [];
+						return { ...prev, [d.channel]: [...list, d] };
 					});
+
+					if (d.channel === selected) scrollMessages();
 					break;
 				case 'MESSAGE_EDIT':
 					setMessages(prev => {
@@ -130,12 +162,13 @@ const Channels: React.FC<Props> = ({ user }) => {
 
 	if (!user) return <Redirect to="/login" />;
 
-	if (!connected)
+	if (!connected) {
 		return (
 			<main style={{ padding: 0 }}>
 				<ConnectingText />
 			</main>
 		);
+	}
 
 	return (
 		<main style={{ padding: 0 }} id="channels-container">
@@ -155,18 +188,35 @@ const Channels: React.FC<Props> = ({ user }) => {
 			<div id="channel-messages">
 				{!(selected in messages) ? (
 					<Loading />
-				) : !messages[selected].length ? (
-					<p id="no-messages-text" className="italic">
-						This channel doesn't have any messages yet!
-					</p>
 				) : (
 					<>
-						{messages[selected].map(message => (
-							<div key={message.id} className="message">
-								<span className="bold">{message.author.username}:</span>{' '}
-								{message.content}
-							</div>
-						))}
+						<div id="messages" ref={messagesRef}>
+							{!messages[selected].length ? (
+								<p id="no-messages-text" className="italic">
+									This channel doesn't have any messages yet!
+								</p>
+							) : (
+								<>
+									{messages[selected].map(message => (
+										<div key={message.id} className="message">
+											<span className="bold">{message.author.username}:</span>{' '}
+											{message.content}
+										</div>
+									))}
+								</>
+							)}
+						</div>
+						<div id="message-input">
+							<input
+								type="text"
+								placeholder={`Message #${channels[selected].name}`}
+								value={input}
+								autoFocus
+								onChange={e => setInput(e.target.value.trimStart())}
+								onKeyPress={handleKeyPress}
+								ref={inputRef}
+							/>
+						</div>
 					</>
 				)}
 			</div>
